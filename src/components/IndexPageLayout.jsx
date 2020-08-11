ad44ffa3f609a3973609f6b53baa7b8bf7c662f3
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-12 01:11:12 +0700

    update layout

--- src/components/IndexPageLayout.jsx
+++ src/components/IndexPageLayout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { BackTop } from 'antd'
 import Heading from './Heading'
+import About from './About'
 
 const IndexPageLayout = () => (
   <div>
+    <BackTop />
     <Heading />
+    <About />
   </div>
 )
 
