1da2bbb3a3646dc6a47824d853628e11db367392
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-12 01:12:12 +0700

    add about

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './About.module.css'
+
+const Highlight = () => (
+  <div className={style.root}>
+    <h2>About</h2>
+    <div>New graduate. Web developer. Self learner. Can improve. Never perfect</div>
+  </div>
+)
+
+export default Highlight
