e718b0ff8081a556f4047e33995e9d71615581fa
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-12 01:12:03 +0700

    add social link

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { LinkedinFilled, FacebookFilled, GithubFilled } from '@ant-design/icons'
+import SocialLink from './SocialLink'
 import style from './Heading.module.css'
 
 const Heading = () => (
   <div className={style.root}>
     <h1>Poosarn Moolmuang</h1>
     <h3>@phumoonlight</h3>
+    <SocialLink icon={<LinkedinFilled />} url="https://www.linkedin.com/in/poosarn-moolmuang/" />
+    <SocialLink icon={<FacebookFilled />} url="https://www.facebook.com/ppmeemee" />
+    <SocialLink icon={<GithubFilled />} url="https://github.com/phumoonlight/" />
   </div>
 )
 
