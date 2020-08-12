c77166f6b38ef9d89be3b7819e0930de84e69e3e
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-12 17:46:06 +0700

    update heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,6 +5,7 @@ import style from './Heading.module.css'
 
 const Heading = () => (
   <div className={style.root}>
+    <img className={style.profileimg} src="/profile.png" alt="profile" />
     <h1>Poosarn Moolmuang</h1>
     <h3>@phumoonlight</h3>
     <SocialLink icon={<LinkedinFilled />} url="https://www.linkedin.com/in/poosarn-moolmuang/" />
