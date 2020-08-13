a2d9f6d2c7d23d069fadb229c4f2c3426bb334bb
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-13 17:01:43 +0700

    remove iframe

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,6 @@ import style from './Work.module.css'
 const Work = () => (
   <div className={style.root}>
     <h2>MY WORK 🔨</h2>
-    <iframe src="https://phumoonlight.github.io/" allowFullScreen title="W3Schools Free Online Web Tutorials" />
   </div>
 )
 
