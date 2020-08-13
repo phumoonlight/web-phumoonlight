8770ea5a5417a7e69396ccbe4ae605bea2d9315d
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-13 18:27:15 +0700

    add repo from github to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Axios from 'axios'
 import style from './Work.module.css'
 
-const Work = () => (
-  <div className={style.root}>
-    <h2>MY WORK 🔨</h2>
-  </div>
-)
+const Work = () => {
+  const [githubRepos, setGithubRepos] = React.useState([])
+  React.useEffect(() => {
+    const fetchGithubRepos = async () => {
+      const { data } = await Axios.get('https://api.github.com/users/phumoonlight/repos')
+      setGithubRepos(data)
+    }
+    fetchGithubRepos()
+  })
+  return (
+    <div className={style.root}>
+      <h2>MY WORK (WEBSITE) 🔨</h2>
+      <div className={style.workcontainer}>
+        {githubRepos.length && githubRepos
+          .filter((repo) => repo.name.includes('web'))
+          .map((repo) => (
+            <a href={repo.html_url} className={style.repobox}>
+              <div>{repo.name}</div>
+              <div>{repo.description}</div>
+            </a>
+          ))}
+      </div>
+    </div>
+  )
+}
 
 export default Work
