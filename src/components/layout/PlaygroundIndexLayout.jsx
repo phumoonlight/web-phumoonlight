1924883b468c48a67c9b433d784e7e556247314e
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-13 23:02:02 +0700

    init page /playground

---
+++ src/components/layout/PlaygroundIndexLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BackTop } from 'antd'
+
+const IndexLayout = () => (
+  <div>
+    <BackTop />
+  </div>
+)
+
+export default IndexLayout
---
+++ src/pages/playground/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import IndexPageLayout from '../../components/layout/IndexPageLayout'
+
+const IndexPage = () => <IndexPageLayout />
+
+export default IndexPage
