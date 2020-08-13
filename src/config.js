fdf8850abb9b13b7b04c9ea91a9ca4b600d82cf8
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-13 23:46:03 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 export const GITHUB_PROFILE_URL = 'https://phumoonlight.github.io/'
 export const GITHUB_API_URL_REPOS = 'https://api.github.com/users/phumoonlight/repos'
 export const HEAD = {
-  title: 'Phumoonlight | Profile',
+  title: {
+    index: 'Phumoonlight | Home',
+    profile: 'Phumoonlight | Profile',
+    playground: 'Phumoonlight | Playground',
+  },
   meta: {
     description: 'phumoonlight profile',
     keywords: 'phumoonlight, phumoonlight profile',
