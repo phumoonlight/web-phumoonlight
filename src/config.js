0191b139a73b531737316f42d9488e8a8f21d749
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-13 17:01:48 +0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const GITHUB_PROFILE_URL = 'https://phumoonlight.github.io/'
+export const HEAD = {
+  title: 'Phumoonlight | Profile',
+  meta: {
+    description: 'phumoonlight profile',
+    keywords: 'phumoonlight, phumoonlight profile',
+    author: 'Poosarn Moolmuang',
+    googleVerification: 'D0xKM0COReiNULH2W76w5XU7RidESgRIDtN8PWZHZfc',
+  },
+}
