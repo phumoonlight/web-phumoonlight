0d3431851ea1b7244fe89a91511e5a6929dcf2d3
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-15 15:16:14 +0700

    profile: use smaller image

--- src/features/profile/ProfileLayout.jsx
+++ src/features/profile/ProfileLayout.jsx
@@ -5,7 +5,7 @@ import Detail from './components/Detail'
 const ProfileLayout = () => (
   <div>
     <Nav label="Profile" />
-    <Heading backgroundURL="/images/profile-main-banner.png">
+    <Heading backgroundURL="/images/profile-main-banner.jpg">
       <span>Poosarn Moolmuang</span>
     </Heading>
     <Detail />
