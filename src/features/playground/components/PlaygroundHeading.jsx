6d5f243ccca5dd0d9e217119f8aa819c1a0166c9
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-16 10:51:51 +0700

    rename, fix style

---
+++ src/features/playground/components/PlaygroundHeading.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const PlaygroundHeading = styled.h1`
+  text-align: center;
+  font-weight: bold;
+  padding-bottom: 25px;
+  color: ${(props) => props.color}
+`
+
+export default PlaygroundHeading
