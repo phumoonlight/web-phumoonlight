ef4cf2ffbad9e86865235436cbc3f9bc97a09f00
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-16 10:52:24 +0700

    add context provider

--- src/pages/playground/compressimg.jsx
+++ src/pages/playground/compressimg.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Head from 'next/head'
 import { HEAD } from '../../config'
 import PlaygroundFunctionLayout from '../../features/playground/PlaygroundFunctionLayout'
+import PlaygroundFunctionContext from '../../features/playground/PlaygroundFunctionContext'
 
 const PlaygroundCompressImg = () => (
-  <>
+  <PlaygroundFunctionContext.Provider value={{ title: 'CompressImg' }}>
     <Head>
       <title>{HEAD.title.playgroundCompressImg}</title>
     </Head>
-    <PlaygroundFunctionLayout extendedTitle="CompressImg" />
-  </>
+    <PlaygroundFunctionLayout />
+  </PlaygroundFunctionContext.Provider>
 )
 
 export default PlaygroundCompressImg
