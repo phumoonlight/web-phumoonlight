9774c8c2ff20710740ce341891855b926f562ddf
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-16 12:17:29 +0700

    use styled-components, remove css, add antd tooltip

--- src/common/components/Network.jsx
+++ src/common/components/Network.jsx
@@ -1,20 +1,51 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Tooltip } from 'antd'
 import { LinkedinFilled, FacebookFilled, GithubFilled } from '@ant-design/icons'
 import LinkNewTab from './LinkNewTab'
-import style from './Network.module.css'
 
-const Network = () => (
-  <div className={style.root}>
+const Network = ({ className }) => (
+  <div className={className}>
     <LinkNewTab href="https://www.linkedin.com/in/poosarn-moolmuang/">
-      <LinkedinFilled />
+      <Tooltip title="Visit my LinkedIn">
+        <LinkedinFilled />
+      </Tooltip>
     </LinkNewTab>
     <LinkNewTab href="https://www.facebook.com/ppmeemee">
-      <FacebookFilled />
+      <Tooltip title="Visit my Facebook">
+        <FacebookFilled />
+      </Tooltip>
     </LinkNewTab>
     <LinkNewTab href="https://github.com/phumoonlight/">
-      <GithubFilled />
+      <Tooltip title="Visit my GitHub">
+        <GithubFilled />
+      </Tooltip>
     </LinkNewTab>
   </div>
 )
 
-export default Network
+Network.propTypes = {
+  className: PropTypes.string.isRequired,
+}
+
+export default styled(Network)`
+  display: flex;
+  justify-content: center;
+  background: #dadada75;
+  position: relative;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  a {
+    font-size: 50px;
+    margin-left: 25px;
+    margin-right: 25px;
+    color: #414141;
+    transition: .15s;
+  }
+  a:hover {
+    color: #0066ff;
+    transform: scale(1.05);
+  }
+`
