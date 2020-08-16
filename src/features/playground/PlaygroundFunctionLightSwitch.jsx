8d565253ba9d6ba12042df000fccedf3482f9fdb
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-16 10:52:07 +0700

    add heading

--- src/features/playground/PlaygroundFunctionLightSwitch.jsx
+++ src/features/playground/PlaygroundFunctionLightSwitch.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Switch } from 'antd'
+import { PlaygroundHeading } from './components'
 
 const PlaygroundFunctionLightSwitch = ({ className }) => {
   const [islightOn, setIsLightOn] = React.useState(true)
   const toggleLight = () => setIsLightOn((prev) => !prev)
   return (
     <div className={className} style={{ background: islightOn ? 'white' : '#333945' }}>
+      <PlaygroundHeading color={islightOn ? 'black' : 'white'}>
+        LIGHT SWITCH
+      </PlaygroundHeading>
       <Switch defaultChecked onChange={toggleLight} />
     </div>
   )
