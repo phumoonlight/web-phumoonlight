ce6530b59df29a47ec8eecc4a01722d834629330
Phusan Moonmuang <phumoonlight@hotmail.com>
2021-02-07 22:08:51 +0700

    feat: facebook chat plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import MessengerCustomerChat from 'react-messenger-customer-chat'
 import { PageRoot } from '../components/PageRoot'
 import { IndexLayout } from '../layouts/IndexLayout'
 import { HEAD } from '../config'
@@ -10,6 +11,10 @@ const IndexPage: React.FC = () => (
       <title>{HEAD.title.index}</title>
     </Head>
     <IndexLayout />
+    <MessengerCustomerChat
+      pageId="307397203233474"
+      appId="170703504823036"
+    />
   </PageRoot>
 )
 
